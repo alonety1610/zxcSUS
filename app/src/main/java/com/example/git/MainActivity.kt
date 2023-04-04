735b3b932a6e9d3ab32bb70e9db528d2520f4fe4
Admin <alesekovkirill@gmail.com>
2023-04-04 17:22:32 +0500

    dfgsg

--- app/src/main/java/com/example/git/MainActivity.kt
+++ app/src/main/java/com/example/git/MainActivity.kt
@@ -7,7 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val x = 5
+        val vv = 12.1
     }
 
 }
